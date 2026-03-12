6ac9003a8ad3097febae21cdc4b867c3c9322f99
wnghks2516 <wnghks2516@naver.com>
2026-03-12 16:58:41 +0900

    Feat ( InputSystem ) : Move 키 입력 테스트 및 디버그 메시지 출력

---
+++ Assets/@Script/InputSystem/InputHandler.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+/* 스텍 (Stack )
+ * 힙(Heap)
+ * 
+ * 스텍은 함수의 매개변수, 지역 변수, 리턴 주소 등을 저장하는 메모리 영역입니다.
+ * 스텍은 LIFO(Last In First Out) 방식으로 작동하며, 
+ * 함수가 호출될 때마다 새로운 스텍 프레임이 생성되고,
+ * 함수가 종료되면 해당 프레임이 제거됩니다. 
+ * 스텍은 빠른 접근 속도를 제공하지만, 크기가 제한되어 있습니다.
+ * 스텍의 예시 = int, float, bool, char, struct 등과 같은 값 형식 (Value Type) 
+ * 
+ * 
+ * 힙이란 동적으로 할당되는 메모리 영역입니다.
+ * 힙의 예시 = Class 
+ */
+
+
+
+namespace WarriorQuest.InputSystem
+{
+
+    public class InputHandler : MonoBehaviour
+    {
+        //inputsystem_Action의 인스턴스
+        private InputSystem_Actions inputSystemAction;
+
+        // 액션 참조 변수
+        private InputAction MoveAction;
+        private InputAction AttackAction;
+        private InputAction IntractAction;
+
+
+        #region 유니티 생명주기 함수
+        private void Awake()
+        {
+            inputSystemAction = new InputSystem_Actions();
+
+            MoveAction = inputSystemAction.Player.Move;
+            AttackAction  = inputSystemAction.Player.Attack;
+            IntractAction = inputSystemAction.Player.Interact;
+        }
+        private void OnEnable()
+        {
+            //입력시스템을 활성화
+
+            //performed = 입력이 발생했을 때 실행되는 이벤트
+            //Started = 입력이 시작될 때 실행되는 이벤트
+            //Canceled = 입력이 취소될 때 실행되는 이벤트
+            inputSystemAction.Enable();
+            MoveAction.performed += OnMove;
+            MoveAction.canceled += OnMove;
+        }
+
+        private void OnDisable()
+        {
+            //입력시스템을 비활성화
+            inputSystemAction.Disable();
+            MoveAction.performed -= OnMove;
+            MoveAction.canceled -= OnMove;
+        }
+        #endregion
+
+        //콜백 매서드 - 입력이 발생했을 때 실행되는 매서드
+        #region 콜백 매서드
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            Debug.Log($"Move Input : {context.ReadValue<Vector2>()}");
+        }
+        #endregion
+
+    }
+
+}
